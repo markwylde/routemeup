58a7337fcc7e878443dab78fee616cea87f40262
korynunn <knunn187@gmail.com>
2020-04-21 10:48:09 +1000

    Fixed matching

--- index.js
+++ index.js
@@ -6,24 +6,23 @@ function routemeup (controllers, { method, url }) {
 
     if (matched) {
       const controller = controllers[route];
-      
+
+
       if(typeof controller === 'function'){
         return {
           controller: controller,
           tokens: matched.params
         };
       }
-      
+
       if(method){
         const requestMethod = method.toUpperCase();
         const controllerMethod = Object.keys(controller).find(key => key.toUpperCase() === requestMethod);
 
-        if (controller[controllerMethod]) {
-          return {
-            controller: controller[method],
-            tokens: matched.params
-          };
-        }
+        return {
+          controller: controller[controllerMethod],
+          tokens: matched.params
+        };
       }
     }
   }
